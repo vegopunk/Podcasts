d42e1428356a527a3e696c991cc24530fc7e7d33
vegopunk <vegopunk@mail.ru>
2018-02-14 12:26:13 +0300

    ep.2 Начало проекта
    Создание проекта. Добавление трех вкладок. Разбиение все по функциям.

---
+++ Podcasts/MainTabBarController.swift
@@ -0,0 +1,54 @@
+//
+//  MainTabBarController.swift
+//  Podcasts
+//
+//  Created by Денис Попов on 14.02.2018.
+//  Copyright © 2018 Денис Попов. All rights reserved.
+//
+
+import UIKit
+
+class MainTabBarController : UITabBarController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        UINavigationBar.appearance().prefersLargeTitles = true
+        
+        tabBar.tintColor = .purple
+        
+        setupViewController()
+        
+    }
+    
+    //MARK:- Setup Functions
+    
+    func setupViewController() {
+        viewControllers = [
+            generateNavigationController(for: ViewController(), title: "Favorites", image: #imageLiteral(resourceName: "favorites")),
+            generateNavigationController(for: ViewController(), title: "Search", image: #imageLiteral(resourceName: "search")),
+            generateNavigationController(for: ViewController(), title: "Downloads", image: #imageLiteral(resourceName: "downloads"))
+        ]
+    }
+    
+    //MARK:- Helper Functions
+    
+    fileprivate func generateNavigationController(for rootViewController: UIViewController , title : String , image : UIImage) -> UIViewController {
+        
+        let navController = UINavigationController(rootViewController: rootViewController)
+//        navController.navigationBar.prefersLargeTitles = true
+        rootViewController.navigationItem.title = title
+        navController.tabBarItem.title = title
+        navController.tabBarItem.image = image
+        
+        return navController
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
