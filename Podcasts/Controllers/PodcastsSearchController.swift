b78f72b6b6cab2e117e3d318a99998e1ac0e5a32
vegopunk <vegopunk@mail.ru>
2018-02-15 10:55:02 +0300

    ep.3 Создание ячеек
    Создание ячеек в вкладке поиска. Добавление строки поиска в навбар.

---
+++ Podcasts/Controllers/PodcastsSearchController.swift
@@ -0,0 +1,74 @@
+//
+//  PodcastSearchController.swift
+//  Podcasts
+//
+//  Created by Денис Попов on 15.02.2018.
+//  Copyright © 2018 Денис Попов. All rights reserved.
+//
+
+import UIKit
+
+class PodcastsSearchController : UITableViewController,UISearchBarDelegate {
+    
+    
+    let podcasts = [
+        Podcast(name: "Some Podcast", artistName: "Denis Voong"),
+        Podcast(name: "Lets Build that app", artistName: "Brian Voong"),
+        Podcast(name: "vego.pro", artistName: "Brian Voong")
+    ]
+    
+    let cellId = "cellId"
+    
+    //search controlelr
+    
+    let searchController = UISearchController(searchResultsController: nil)
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupSearchBar()
+        setupTableView()
+    }
+    
+    //MARK:- Setup Work
+    
+    fileprivate func setupSearchBar() {
+        
+        //добавление searchController
+        navigationItem.searchController = searchController
+        //всегда показывать строку поиска
+        navigationItem.hidesSearchBarWhenScrolling = false
+        //убирает title когда нажимаем на строку поиска
+        searchController.dimsBackgroundDuringPresentation = false
+        //получать текст из строки поиска
+        searchController.searchBar.delegate = self
+    }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        print(searchText)
+        //дальше здесь будет реализован поиск с помощью Alamofire по itunes API
+    }
+    
+    fileprivate func setupTableView() {
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellId)
+    }
+    
+    //MARK:- UITableView
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return podcasts.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath)
+        
+        let podcast = self.podcasts[indexPath.row]
+        
+        cell.textLabel?.text = "\(podcast.name) \n\(podcast.artistName)"
+        cell.textLabel?.numberOfLines = -1
+        
+        cell.imageView?.image = #imageLiteral(resourceName: "appicon")
+        
+        return cell
+    }
+    
+}
---
+++ Podcasts/Models/Podcast.swift
@@ -0,0 +1,15 @@
+//
+//  Podcast.swift
+//  Podcasts
+//
+//  Created by Денис Попов on 15.02.2018.
+//  Copyright © 2018 Денис Попов. All rights reserved.
+//
+
+import Foundation
+
+
+struct Podcast {
+    let name : String
+    let artistName : String
+}
