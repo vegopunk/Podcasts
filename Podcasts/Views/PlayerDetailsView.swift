6694fb46b7b3e78d9b10391e800c209394c8b11f
vegopunk <vegopunk@mail.ru>
2018-03-02 17:20:05 +0300

    ep.15 AVPlayer
    Сделано воспроизведение подкастов с помощью AVKit. Работает кнопка play/pause.

--- Podcasts/Views/PlayerDetailsView.swift
+++ Podcasts/Views/PlayerDetailsView.swift
@@ -7,21 +7,57 @@
 //
 
 import UIKit
+import AVKit
 
 class PlayerDetailsView: UIView {
     
     var episode : Episode! {
         didSet{
             titleLabel.text = episode.title
-            
             authorLabel.text = episode.author
+            
+            playEpisode()
             
             guard let url = URL(string: episode.imageUrl ?? "") else {return}
             episodeImageView.sd_setImage(with: url)
         }
     }
     
-    @IBOutlet weak var playPauseButton: UIButton!
+    fileprivate func playEpisode() {
+        print("trying to play episode at url: " , episode.streamUrl)
+        
+        guard let url = URL(string: episode.streamUrl) else {return}
+        let playerItem = AVPlayerItem(url: url)
+        player.replaceCurrentItem(with: playerItem)
+        player.play()
+    }
+    
+    let player : AVPlayer = {
+        let avPlayer = AVPlayer()
+        avPlayer.automaticallyWaitsToMinimizeStalling = false
+        return avPlayer
+    }()
+    
+    //MARK:- IB Actions and Outlets
+    
+    @IBOutlet weak var playPauseButton: UIButton! {
+        didSet{
+            playPauseButton.setImage(#imageLiteral(resourceName: "pause"), for: .normal)
+            playPauseButton.addTarget(self ,  action: #selector(handlePlayPause), for: .touchUpInside)
+        }
+    }
+    
+    @objc func handlePlayPause() {
+        print("Trying to play and pause")
+        
+        if player.timeControlStatus == .paused {
+            player.play()
+            playPauseButton.setImage(#imageLiteral(resourceName: "pause"), for: .normal)
+        } else {
+            player.pause()
+            playPauseButton.setImage(#imageLiteral(resourceName: "play"), for: .normal)
+        }
+    }
     
     @IBOutlet weak var authorLabel: UILabel!
     
