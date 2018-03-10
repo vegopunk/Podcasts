1235b8d70a68ba7b6b61c9ee37083b6efec04e50
vegopunk <vegopunk@mail.ru>
2018-03-10 11:46:06 +0300

    ep.23 Drag and drop UIPanGestureRecognizer
    Сделана анимация , когда мы тянем за miniPlayer. По мере вытягивания изменяется значение alpha для обоих плееров.

--- Podcasts/Views/PlayerDetailsView.swift
+++ Podcasts/Views/PlayerDetailsView.swift
@@ -67,6 +67,7 @@ class PlayerDetailsView: UIView {
         super.awakeFromNib()
         
         addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleTapMaximize)))
+        addGestureRecognizer(UIPanGestureRecognizer(target: self, action: #selector(handlePan)))
         
         observePlayerCurrentTime()
         
@@ -82,6 +83,27 @@ class PlayerDetailsView: UIView {
             self?.enlargeEpisodeImageVIew()
         }
         
+    }
+    
+    @objc func handlePan(gesture: UIPanGestureRecognizer) {
+//        print("Panning")
+        if gesture.state == .began {
+            print("began")
+        } else if gesture.state == .changed {
+            let translation = gesture.translation(in: self.superview)
+            self.transform = CGAffineTransform(translationX: 0, y: translation.y)
+            
+            self.miniPlayerView.alpha = 1 + translation.y / 200
+            self.maximizedStackVIew.alpha = -translation.y / 200
+            
+            print(translation.y)
+        } else if gesture.state == .ended {
+            UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 0.7, initialSpringVelocity: 1, options: .curveEaseOut, animations: {
+                self.transform = .identity
+                self.miniPlayerView.alpha = 1
+                self.maximizedStackVIew.alpha = 0
+            })
+        }
     }
     
     @objc func handleTapMaximize() {
